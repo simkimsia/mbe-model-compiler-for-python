231ccd4a7f18f1ea84c02400427dae94a7dd6c8e
KimSia Sim <kimsia@oppoin.com>
2020-09-08 22:08:08 +0800

    Rename to Python37 and Django22

--- src/Django22Output.java
+++ src/Django22Output.java
@@ -2,7 +2,7 @@ import java.io.FileOutputStream;
 import java.io.OutputStream;
 import java.io.PrintStream;
 
-public class DjangoOutput extends CodeOutput {
+public class Django22Output extends CodeOutput {
     // General description
     // Model compiler
     // This is a helper class to manage indentation in generated code
